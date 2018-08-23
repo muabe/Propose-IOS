9964caa27bdde095c6477d265a5e91762a9abe4a
박길남 <gilnam_park@genieworks.net>
2018-08-23 10:20:43 +0900

    초기 프로젝트 등록

---
+++ propose-ios/propose-ios/propose_ios.h
@@ -0,0 +1,19 @@
+//
+//  propose_ios.h
+//  propose-ios
+//
+//  Created by 박길남 on 2018. 8. 23..
+//  Copyright © 2018년 swieeft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for propose_ios.
+FOUNDATION_EXPORT double propose_iosVersionNumber;
+
+//! Project version string for propose_ios.
+FOUNDATION_EXPORT const unsigned char propose_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <propose_ios/PublicHeader.h>
+
+
---
+++ propose-ios/propose-iosTests/propose_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  propose_iosTests.swift
+//  propose-iosTests
+//
+//  Created by 박길남 on 2018. 8. 23..
+//  Copyright © 2018년 swieeft. All rights reserved.
+//
+
+import XCTest
+@testable import propose_ios
+
+class propose_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
